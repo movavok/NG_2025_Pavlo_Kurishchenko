0e49f56e3ff21d817836115cfb50df7303cb6795
«movavok» <pkurishchenkoukr.net05@gmail.com>
2025-05-09 13:38:29 +0300

    Task 1.2 was modified to remove copypasta in the code

--- Lesson_1/Task_2/calculator.cpp
+++ Lesson_1/Task_2/calculator.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void calculate(double a, double b, char oper, double result){ cout << "Your result is: " << a << oper << b << " = " << result << endl;}
+
 int main()
 {
     double first_num = 0;
@@ -10,41 +12,33 @@ int main()
 
     cout << "Thats a simple calculator!\n"
             "Enter 2 whole numbers and an operation between them (like 2 + 3, use spaces to separate numbers and operation)\n"
-            "'+' - plus\n"
-            "'-' - minus\n"
-            "'*' - multiplication\n"
-            "'/' - division\n"
-            "'^' - power->(first number to second number power)\n"
-            "'v' - square root->(enter only one number and symbol of this operation)\n\n"
-            ">> ";
-    cin >> first_num >> operation >> second_num;
+            "'+'  - plus\n"
+            "'-'  - minus\n"
+            "'*'  - multiplication\n"
+            "'/'  - division\n"
+            "'^'  - power->(first number to second number power)\n"
+            "'v2' - square root->(enter only your number and symbol of this operation)\n\n";
 
-    if (operation == 'v'){
-        cout << "Your result is: " << first_num << operation << " = " << pow(first_num, 0.5) << endl;
-        if (second_num != 0)
-            cout << "You should not enter second number if you choose square root operation, so i did not use that value ,_," << endl;
-    }
-    else if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^') {
-        switch (operation){
-        case '+':
-            cout << "Your result is: " << first_num << operation << second_num << " = " << first_num + second_num << endl;
-            break;
-        case '-':
-            cout << "Your result is: " << first_num << operation << second_num << " = " << first_num - second_num << endl;
-            break;
-        case '*':
-            cout << "Your result is: " << first_num << operation << second_num << " = " << first_num * second_num << endl;
-            break;
-        case '/':
-            cout << "Your result is: " << first_num << operation << second_num << " = " << first_num / second_num << endl;
-            break;
-        case '^':
-            cout << "Your result is: " << first_num << operation << second_num << " = " << pow(first_num, second_num) << endl;
-            break;
+    while (true){
+        cout << ">> ";
+        cin >> first_num >> operation >> second_num;
+
+        if (operation == 'v'){ // square root
+            if (second_num != 2) cerr << "Incorrect symbol ,_," << endl;
+            else calculate(first_num, 2, operation, pow(first_num, 0.5));
         }
-    }
-    else{
-        cout << "You entered non-exist operation or number ,_," << endl;
+        else if (operation == '+' || operation == '-' || operation == '*' || operation == '/' || operation == '^') { // other operations
+            double result = 0;
+            switch (operation){
+            case '+': result = first_num + second_num; break;
+            case '-': result = first_num - second_num; break;
+            case '*': result = first_num * second_num; break;
+            case '/': result = first_num / second_num; break;
+            case '^': result = pow(first_num, second_num); break;
+            }
+            calculate(first_num, second_num, operation, result);
+        }
+        else cerr << "You entered non-exist operation or number ,_," << endl;
     }
     return 0;
 }
